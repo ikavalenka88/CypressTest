dd5c23b1370c0b87c82513663d39ebf9585158e5
ikavalenka88 <z.yellow@yandex.by>
2023-06-11 19:45:14 +0200

    add comment

--- cypress/e2e/udemyPractice/firstTest.cy.js
+++ cypress/e2e/udemyPractice/firstTest.cy.js
@@ -10,9 +10,8 @@ it.only('Using GET and FIND and Eq',() =>{
     cy.viewport(1800,700)
     cy.visit('https://www.cypress.io/blog/')
     cy.get('div').find('div').find('div').find('div').find('a').eq(0)
-
 })
-
+//Comment
 
 // describe('Mobile phone replenishment', () =>{
 // it('Check min sum', () =>{
