2c2df05ee1f2ba3799d031a14e4c2ceaaca0e1e2
ikavalenka88 <z.yellow@yandex.by>
2023-06-13 01:33:42 +0200

    add lecture 17

--- cypress/e2e/udemyPractice/spec0.cy.js
+++ cypress/e2e/udemyPractice/spec0.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+///<reference types = "Cypress"/>
+
+
+it('Lesson17', () =>{
+  cy.visit('https://next.privat24.ua/mobile?lang=en')
+  cy.contains('Sign in')
 })
+
+it('Lesson17', () =>{
+  cy.visit('https://next.privat24.ua/mobile?lang=en')
+  cy.contains('div','Sign in')
+})
+
+it('Lesson17', () =>{
+  cy.visit('https://next.privat24.ua/mobile?lang=en')
+  cy.contains('SIGN IN',{matchCase:false})
+})
+
+it.only('Lesson17', () =>{
+  cy.visit('https://next.privat24.ua/mobile?lang=en')
+  cy.get('footer').contains('API')
+})
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
